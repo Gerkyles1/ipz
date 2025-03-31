f269afc26cba2e0dde1e0c2a204afce437a0f2de
Gerkyles1 <maxfast333@gmail.com>
2025-03-31 22:57:47 +0200

    initial commit

---
+++ Assets/Scenes/LevelsChooser.cs
@@ -0,0 +1,39 @@
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LevelsChooser : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
+    void Start()
+    {
+        int maxLevel;
+        SaveManager.data.MaxLevel = 1;
+        maxLevel = SaveManager.data.MaxLevel;
+
+
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            Match levelNum = Regex.Match(child.name, @"\((\d+)\)");
+            if (levelNum.Success)
+            {
+                if (int.Parse(levelNum.Groups[1].Value) <= maxLevel)
+                {
+                    child.GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        audioSource.PlayOneShot(audioClip);
+                        //SceneManager.LoadScene(int.Parse(levelNum.Groups[1].Value));
+                        Debug.Log("scene load" + int.Parse(levelNum.Groups[1].Value));
+                    });
+                }
+                else
+                    child.GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/SaveManager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using UnityEditor;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    private static readonly string saveFilePath = Path.Combine(Application.persistentDataPath, "saveData.json");
+
+    public static SaveData data { get; private set; } = new SaveData();
+
+
+
+    public static void Save()
+    {
+        string json = JsonUtility.ToJson(data);
+
+
+        File.WriteAllText(saveFilePath, json);
+        Debug.Log(saveFilePath);
+
+    }
+
+    public static void Load()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string json = File.ReadAllText(saveFilePath);
+
+            data = JsonUtility.FromJson<SaveData>(json);
+        }
+    }
+}
+
+[System.Serializable]
+public class SaveData
+{
+    [SerializeField] private int maxlevel = 1;
+    public int MaxLevel
+    {
+        get 
+        {
+            SaveManager.Load();
+            return maxlevel; 
+        }
+
+        set
+        {
+            maxlevel = System.Math.Clamp(value, 0, 18);
+            SaveManager.Save();
+        }
+    }
+}
